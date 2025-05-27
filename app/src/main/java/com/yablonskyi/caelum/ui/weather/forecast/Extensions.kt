5e4d54e38878a645da27c88f71d4f529785e5d06
Daniel <daniilyablonskiy88@gmail.com>
2025-05-27 17:56:02 +0200

    add function to detect night

--- app/src/main/java/com/yablonskyi/caelum/ui/weather/forecast/Extensions.kt
+++ app/src/main/java/com/yablonskyi/caelum/ui/weather/forecast/Extensions.kt
@@ -2,13 +2,14 @@ package com.yablonskyi.caelum.ui.weather.forecast
 
 import com.yablonskyi.caelum.R
 import java.text.SimpleDateFormat
+import java.time.Instant
+import java.time.ZoneOffset
 import java.util.Date
 import java.util.Locale
 import java.util.TimeZone
 
 fun String.toIcon(description: String, time: Long, sunrise: Long, sunset: Long, timezone: Long): Int {
-    val now = time + timezone
-    val isNight = now < (sunrise + timezone) || now > (sunset + timezone)
+    val isNight = isNight(time, sunrise, sunset, timezone)
 
     return when (this) {
         "Clouds" -> {
@@ -38,4 +39,14 @@ fun Long.toHourMinuteTime(timeZoneOffsetSeconds: Long = 0L): String {
     val format = SimpleDateFormat("HH:mm", Locale.getDefault())
     format.timeZone = TimeZone.getTimeZone("UTC")
     return format.format(date)
+}
+
+private fun isNight(time: Long, sunrise: Long, sunset: Long, timezone: Long): Boolean {
+    val offset = ZoneOffset.ofTotalSeconds(timezone.toInt())
+
+    val nowTime = Instant.ofEpochSecond(time).atOffset(offset).toLocalTime()
+    val sunriseTime = Instant.ofEpochSecond(sunrise).atOffset(offset).toLocalTime()
+    val sunsetTime = Instant.ofEpochSecond(sunset).atOffset(offset).toLocalTime()
+
+    return nowTime.isBefore(sunriseTime) || nowTime.isAfter(sunsetTime)
 }
